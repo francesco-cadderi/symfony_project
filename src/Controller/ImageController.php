dd6d05f42a79c17b663e4e850772c0b7006255e3
Insowers <francesco.cadderi@gmail.com.com>
2023-12-02 19:55:19 +0100

    controller works! view works! working on button [stage 4]

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -1,43 +1,36 @@
 <?php
 
-// src/Controller/imageController.php
 namespace App\Controller;
 
 use App\Entity\Image;
-use App\Repository\ImageRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Mapping\Entity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// ...
 
 class ImageController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(EntityManagerInterface $em): Response
     {
-        $repository = $em->getRepository(Image::class);
-        $images = $repository->findAll();
-
-        //dd($images);
-
-        //$images = $imageRepository->findAll();
-    
-        return $this->render('index.html.twig', [
-            'title' => $images[0]->getTitle(),
-            'img' => $images[0]->getImg(),
-            'date' => $images[0]->getDate()->format('Y-m-d H:i:s')
-            
-        ]);
-
-        /* foreach( $images as $singleImage ) {
-            return $this->render('index.html.twig', [
-                'title' => $this->$singleImage->getTitle(),
-                
-            ]);
-        } */
+        $repository = $em->getRepository(Image::class)->findAll();
         
+        $images = array();
+        foreach ($repository as $key => $entity) {
+            $images[$key] = base64_encode(stream_get_contents($entity->getImg()));
+            $titles[$key] = $entity->getTitle();
+            $dates[$key] = $entity->getDate()->format('Y-m-d H:i:s');
+        }
+
+        $bottone = true;
+    
+        return $this->render('index.html.twig', array('repository' => $repository ,
+                                                      'images' => $images,
+                                                      'titles' => $titles,
+                                                      'dates' => $dates,
+                                                      'bottone' => $bottone
+                                                    ));
     }
 }
+
 
